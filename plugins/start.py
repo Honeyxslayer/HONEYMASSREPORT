cc2f4d874909b7622bb8f849f61df7804ebf9184
Honeyxslayer <163923981+Honeyxslayer@users.noreply.github.com>
2025-04-10 07:55:06 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -10,9 +10,9 @@ from info import Config, Txt
 async def handle_start(bot:Client, message:Message):
 
     Btn = [
-        [InlineKeyboardButton(text='🌷 Hᴇʟᴘ 🌷', callback_data='help'), InlineKeyboardButton(text='🌷 Sᴇʀᴠᴇʀ Sᴛᴀᴛs 🌷', callback_data='server')],
-        [InlineKeyboardButton(text='🌷 Uᴘᴅᴀᴛᴇs 🌷', url='https://t.me/Honey_networks'), InlineKeyboardButton(text='🌷 Aʙᴏᴜᴛ 🌷', callback_data='about')],
-        [InlineKeyboardButton(text='🌷 Dᴇᴠᴇʟᴏᴘᴇʀ 🌷', url='https://t.me/OgHoneyy')]
+        [InlineKeyboardButton(text='💀 𝐇ᴇʟᴘ 💀', callback_data='help'), InlineKeyboardButton(text='🍑 𝐒ᴇʀᴠᴇʀ 𝐒ᴛᴀᴛs ☣️', callback_data='server')],
+        [InlineKeyboardButton(text='💀 𝐔ᴘᴅᴀᴛᴇs ☠️', url='https://t.me/Honey_networks'), InlineKeyboardButton(text='⛔ 𝐀ʙᴏᴜᴛ ⛔', callback_data='about')],
+        [InlineKeyboardButton(text='💀 𝐏ᴀᴘᴀ 💀', url='https://t.me/OgHoneyy')]
         ]
 
     await message.reply_text(text=Txt.START_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(Btn))
@@ -21,5 +21,5 @@ async def handle_start(bot:Client, message:Message):
 #Restart to cancell all process 
 @Client.on_message(filters.private & filters.command("restart") & filters.user(Config.SUDO))
 async def restart_bot(b, m):
-    await m.reply_text("💥__Rᴇꜱᴛᴀʀᴛɪɴɢ.....__")
+    await m.reply_text("🏎️💨__𝐑ᴇꜱᴛᴀʀᴛɪɴɢ.....__")
     os.execl(sys.executable, sys.executable, *sys.argv)
